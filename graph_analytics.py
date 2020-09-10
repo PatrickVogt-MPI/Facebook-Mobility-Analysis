17a9edc78b01d6b578a5011191610381978b0136
PatrickVogt-MPI <68254176+PatrickVogt-MPI@users.noreply.github.com>
2020-09-10 05:05:18 +0200

    Add files via upload

--- graph_analytics.py
+++ graph_analytics.py
@@ -1,9 +1,22 @@
 import graph_construction as gc
-import networkx as nx
-from pathlib import Path
 import settings
+from networkx import DiGraph
+from pathlib import Path
+from typing import List
 
-def iterate_over_edges(graph, key=None):
+# list of tuples?
+# search propertys rathe than iterate over properties
+def iterate_over_edges(graph: DiGraph, key: str=None):
+    '''
+    Extracts (specified) properties of all edges in a graph
+
+    Args:
+        graph:  Graph object
+        key:    Dictionary key of an edge property
+
+    Returns:
+        values: List with values of edge property 'key' for all edges in 'graph'. Returns list of property dicts instead, if 'key=None'
+    '''
     values = []
     for node1, node2, data in graph.edges.data():
         if(key): 
@@ -12,7 +25,17 @@ def iterate_over_edges(graph, key=None):
             values.append(data)
     return values
     
-def iterate_over_nodes(graph, key=None):
+def iterate_over_nodes(graph: DiGraph, key:str=None):
+    '''
+    Extracts (specified) properties of all nodes in a graph
+
+    Args:
+        graph:  Graph object
+        key:    Dictionary key of a node property
+
+    Returns:
+        values: List with values of node property 'key' for all nodes in 'graph'. Returns list of property dicts instead, if 'key=None'
+    '''
     values = []
     for node, data in graph.nodes.data():
         if(key): 
@@ -21,7 +44,17 @@ def iterate_over_nodes(graph, key=None):
             values.append(data)
     return values
     
-def iterate_over_graphs(graphs, key=None):
+def iterate_over_graphs(graphs: List[DiGraph], key: str=None):
+    '''
+    Extracts (specified) properties of all graphs in a list of graphs
+
+    Args:
+        graph:  Graph object
+        key:    Dictionary key of a graph property
+
+    Returns:
+        values: List with values of graph property 'key' for all graphs in 'graphs'. Returns list of whole property dicts instead, if 'key=None'
+    '''
     values = []
     for graph in graphs:
         if(key): 
@@ -29,12 +62,16 @@ def iterate_over_graphs(graphs, key=None):
         else: 
             values.append(graph.graph)
     return values
+    
+def adjacency_matrix (graph):
+    pass
 
-graphs = gc.create_movement_graphs_from_csv(settings.settings['data_dir'])
-
-beirut = graphs[0]
-print(iterate_over_edges(beirut, 'n_baseline'))
-print()
-print(iterate_over_nodes(beirut))
-print()
-print(iterate_over_graphs(graphs, 'level'))
+if __name__ == '__main__':
+    graphs = gc.create_movement_graphs_from_csv(settings.settings['data_dir'])
+    
+    for graph in graphs:
+        print(f'Nodes: {len(graph.nodes)}')
+        print(f'Edges: {len(graph.edges)}')
+        print()
+    
+    
--- graph_construction.py
+++ graph_construction.py
@@ -1,48 +1,93 @@
-# Why is 'LINESTRING' different from start_lat, start_lon / end_lon, end_lat? Intrinsic consistency of start/end lat/lon though
+# Why is 'LINESTRING' different from start_lat, start_lon / end_lon, end_lat?
+'''@AlexPompe: 
+the linestring coordinates in the Movement Data sets are the average lat+lon of the starting point
+and ending point of all users counted in the vector - not the centroid
+
+one set of start / end coordinates are the centroid of the polygons and the other are the average starting and ending coordinates of all users who are represented in that row
+'''
 # What is the purpose of 'is_statistically_significant'?
 # Some maps are missing Quadkeys? --> Check for properties
+
+'''
+To do:
+    *only allow valid regular expression for 'key' in  'create_movement_graphs_from_csv'
+    *add path to csv to edge property
+'''
 
 import csv
 import networkx as nx
 from pathlib import Path
-    
-def create_movement_graph(*paths):
+import re
 
-    def add_unique_node(pivot):
+def create_movement_graphs_from_csv(path: Path, key: str=None) -> list:
+    '''
+    Creates a list of graph data structures from all .csv files found in directory <path>
+
+    Args:
+        path:   Path pointing to the data directory of .csv files
+        key:    Search key for files
+        
+    Returns:
+        graphs: List of graph data structures created from each .csv file at the <path> directory. Returns 'None' instead, if empty
+    '''
+
+    def add_unique_node(pivot: dict) -> int:
+        '''
+        Adds node with properties 'pivot' to list 'nodes' if unique and in any case returns its identifier.
+
+        Args:
+            pivot:        Node properties
+
+        Returns:
+            node[0]:      Identifier, node already exists
+            nodes[-1][0]: Identifier, node has been added
+        '''
+        
         for node in nodes:
-            if(node[1]['lat'] == pivot['lat'] and node[1]['lon'] == pivot['lon']): return node[0]
+            if(node[1]['lat'] == pivot['lat'] and node[1]['lon'] == pivot['lon']): 
+                return node[0]
+                
         nodes.append((len(nodes)+1, pivot))
         return nodes[-1][0]
     
     graphs = []
     
-    for path in paths:
+    pattern = '*' + key + '*.csv' if key else '*.csv'
+    csv_paths = [csv_path for csv_path in Path(path).glob(pattern)]
     
-        nodes, edges = [], []
-        
+    for path in csv_paths:
+        # Organizes all data from a .csv file as properties in dicts
         with open(str(path), newline='') as csvfile:
             dict_reader = csv.DictReader(csvfile, delimiter=',')
             
+            edges, nodes = [], []
+            
             for row in dict_reader:
+                coordinates = re.findall(r"[-+]?[0-9]*\.[0-9]*", row['geometry'])
+                
                 graph_properties = {
                     'date_time': row['date_time'],
                     'level':     row['level'],
                     'tile_size': int(row['tile_size'])
                 }
                 start_node_properties = {
-                    'lat':          float(row['start_lat']),
-                    'lon':          float(row['start_lon']),
-                    'country':      row['country'],
+                    'lat':          float(coordinates[0]),
+                    'lon':          float(coordinates[1]),
+                    'polygon_lat':  float(row['start_lat']),
+                    'polygon_lon':  float(row['start_lon']),
                     'polygon_id':   int(row['start_polygon_id']),
                     'polygon_name': row['start_polygon_name'],
+                    'country':      row['country']
                     #'quadkey':      int(row['start_quadkey'])
                 }
                 end_node_properties = {
-                    'lat':          float(row['end_lat']),
-                    'lon':          float(row['end_lon']),
-                    'country':      row['country'],
+                    'lat':          float(coordinates[2]),
+                    'lon':          float(coordinates[3]),
+                    'polygon_lat':  float(row['end_lat']),
+                    'polygon_lon':  float(row['end_lon']),
                     'polygon_id':   int(row['end_polygon_id']),
                     'polygon_name': row['end_polygon_name'],
+                    'country':      row['country']
                     #'quadkey':      int(row['end_quadkey'])
                 }
                 edge_properties = {
@@ -53,11 +98,15 @@ def create_movement_graph(*paths):
                     'z_score':        float(row['z_score']),
                     'percent_change': float(row['percent_change']),
                 }
-            
+                
+                # Adds 'start_node' and 'end_node' to list 'nodes', if unique
                 start_node = add_unique_node(start_node_properties)
-                end_node = add_unique_node(end_node_properties)            
+                end_node = add_unique_node(end_node_properties)
+
+                # Draws edge between 'start_node' and 'end_node'
                 edges.append((start_node, end_node, edge_properties))
-        
+            
+            # Creates graph structure from 'nodes' and 'edges' and adds it to list 'graphs'
             DG = nx.DiGraph(
                 date_time = graph_properties['date_time'], 
                 level     = graph_properties['level'], 
@@ -65,13 +114,10 @@ def create_movement_graph(*paths):
             )
             DG.add_nodes_from(nodes)
             DG.add_edges_from(edges)
+            
             graphs.append(DG)   
-        
-    return graphs    
-
-def create_movement_graphs_from_csv(path):
-    data_dir = [_ for _ in Path(path).glob('*.csv')]
-    return create_movement_graph(*data_dir)
+            
+    return graphs if graphs else None
     
 def save_movement_graph():
     pass
