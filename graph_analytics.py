e924b469b936aff22ba73b15daaaa19f59fa5aa3
PatrickVogt-MPI <68254176+PatrickVogt-MPI@users.noreply.github.com>
2020-09-11 20:27:31 +0200

    Add files via upload

--- graph_analytics.py
+++ graph_analytics.py
@@ -2,44 +2,42 @@ import graph_construction as gc
 import settings
 from networkx import DiGraph
 from pathlib import Path
-from typing import List
 
-# what type hint **kwargs?
-def search_edges(graph: DiGraph, **kwargs):
+def search_edges(graph: DiGraph, **kwargs) -> list:
     '''
-    Searches edges of a graph for property values and returns list of resulting edge identifier
+    Searches edges of a graph for property values and returns list of resulting edges
 
     Args:
         graph:    Graph object
-        **kwargs: edge property key, search value pairs
+        **kwargs: edge property=search value
 
     Returns:
-        edges: list of edge identifier (= node identifier tuples) that fulfill the search criteria
+        edges: list of edges that fulfill the search criteria
     '''
     edges = []
-    for node1, node2, data in graph.edges.data():
+    for id1, id2, data in graph.edges.data():
         if (all(data[key] == value for key, value in kwargs.items())):
-            edges.append((node1, node2))
+            edges.append((id1, id2, data))
     return edges
     
-def search_nodes(graph: DiGraph, **kwargs):
+def search_nodes(graph: DiGraph, **kwargs) -> list:
     '''
-    Searches nodes of a graph for property values and returns list of resulting node identifier
+    Searches nodes of a graph for property values and returns list of resulting nodes
 
     Args:
         graph:    Graph object
-        **kwargs: node property key, search value pairs
+        **kwargs: property key=search value
 
     Returns:
-        nodes: list of node identifier (= natural number) that fulfill the search criteria
+        nodes: list of nodes
     '''
     nodes = []
-    for node, data in graph.nodes.data():
+    for id, data in graph.nodes.data():
         if (all(data[key] == value for key, value in kwargs.items())):
-            nodes.append(node)           
+            nodes.append((id, data))           
     return nodes
     
-def search_graphs(graphs: List[DiGraph], **kwargs):
+def search_graphs(graphs: list, **kwargs) -> list:
     '''
     Searches a graph for property values and returns list of resulting graphs
 
@@ -55,14 +53,33 @@ def search_graphs(graphs: List[DiGraph], **kwargs):
         if (all(graph.graph[key] == value for key, value in kwargs.items())):
             _graphs.append(graph)
     return _graphs
-    
-def adjacency_matrix (graph):
-    pass
 
-if __name__ == '__main__':
-    graphs = gc.create_movement_graphs_from_csv(settings.settings['data_dir'], '', use_global_nodes = False)
-    
+def write_map_maker_coordinates_to_csv(path: str, nodes: list):
+    '''
+    Translates nodes in https://maps.co/ readable coordinates
+
+    Args:
+        path:  path to file containing the coordinates
+        nodes: list of node-tuples of the form (id: int, property: dict)
+    '''
+    coordinates = len(nodes)
+    layers = coordinates // 999 if coordinates % 999 == 0 else coordinates // 999 + 1
+    for layer in range(1, layers + 1):
+        with open(Path(path + str(layer) + '.csv'), 'w') as file:
+            for id, data in nodes[999*(layer-1):999*layer]:
+                file.write(f'{data["lon"]},{data["lat"]}\n')
+
+def print_graph_info(graphs: list):
     for graph in graphs:
+        seperator_length = max(len(f'Graph: {graph.graph["file"]}'), len(f'Nodes: {len(graph.nodes)}'), len(f'Edges: {len(graph.edges)}'))
+        
+        print()
+        print(f'Graph: {graph.graph["file"]}')
+        print('-'*seperator_length)
         print(f'Nodes: {len(graph.nodes)}')
         print(f'Edges: {len(graph.edges)}')
         print()
+            
+if __name__ == '__main__':
+    graphs = gc.create_movement_graphs_from_csv(settings.settings['data_dir'], 'test', use_global_nodes=True, display_runtime=True)
+    print_graph_info(graphs)
--- graph_construction.py
+++ graph_construction.py
@@ -1,30 +1,20 @@
-# NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES NOTES 
-#------------------------------------------------------------------------------------
-# Why is 'LINESTRING' different from start_lat, start_lon / end_lon, end_lat?
-'''@AlexPompe: 
-the linestring coordinates in the Movement Data sets are the average lat+lon of the starting point
-and ending point of all users counted in the vector - not the centroid
-
-one set of start / end coordinates are the centroid of the polygons and the other are the average starting and ending coordinates of all users who are represented in that row
-'''
-# What is the purpose of 'is_statistically_significant'?
-# Some maps are missing Quadkeys? --> Check for properties
-
-'''
-To do:
-    *only allow valid regular expression for 'key' in  'create_movement_graphs_from_csv'
-    *implement save_movement_graph_to_file()
-    *read_movement_graph_from_file()
-'''
-#------------------------------------------------------------------------------------
-#\NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES \NOTES 
+# Definitions:
+# Nodes are defined as tuples: node = (id: int, property: dict)
+#   id is the unique name of the node
+#   property contains the node attributes derived from the .csv data set
+# Edges are defined as 3-tuples: edge = (id1: int, id2: int, property: dict)
+#   id1 is the unique name of the start node
+#   id2 is the unique name of the end node
+#   property contains the edge attributes derived from the .csv data set
 
 import csv
 import networkx as nx
-from pathlib import Path
 import re
+import sys
+from pathlib import Path
+from timeit import default_timer as timer
 
-def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_nodes: bool = False) -> list:
+def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_nodes: bool = False, display_runtime: bool = False) -> list:
     '''
     Creates a list of graph data structures from all .csv files found in directory <path>
 
@@ -35,29 +25,32 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
     Returns:
         graphs: List of graph data structures created from each .csv file at the <path> directory. Returns 'None' instead, if empty
     '''
-
-    def add_unique_node(test_node: dict) -> int:
+    def add_unique_node(t_prop: dict) -> int:
         '''
-        Adds node with properties 'test_node' to list 'global_nodes' if unique,
-        adds it to list 'nodes' for graph construction and returns its identifier.
+        Adds node with properties 't_node' to list 'g_nodes' if unique and in any case
+        adds it to list 'l_nodes' for graph construction and returns its identifier.
+        In this code, t stands for test, g for global and  l for local.
 
         Args:
-            test_node:           Node properties
+            t_node: properties of the test node
 
         Returns:
-            node[0]:             Identifier, node already exists
-            global_nodes[-1][0]: Identifier, node has been added
+            g_id:   identifier of global node
+            t_id:   identifier of local node
         '''
-        for node in global_nodes:
-            if(node[1]['lat'] == test_node['lat'] and node[1]['lon'] == test_node['lon']):
-                local_nodes.append(node)
-                return node[0]
-        global_nodes.append((len(global_nodes)+1, test_node))
-        local_nodes.append(global_nodes[-1])   
-        return global_nodes[-1][0]
+        for g_id, g_prop in g_nodes:
+            if(g_prop['lat'] == t_prop['lat'] and g_prop['lon'] == t_prop['lon']):
+                l_nodes.append((g_id, g_prop))
+                return g_id
+        t_id = len(g_nodes) + 1
+        g_nodes.append((t_id, t_prop))
+        l_nodes.append((t_id, t_prop))
+        return t_id
+    
+    start = timer()
     
     # Introduction of global_nodes causes consistent identifier for all nodes among all graphs from different .csv files
-    graphs, global_nodes, temp = [], [], []
+    graphs, g_nodes, temp = [], [], []
     
     pattern = '*' + key + '*.csv' if key else '*.csv'
     csv_paths = [csv_path for csv_path in Path(path).glob(pattern)]
@@ -66,7 +59,7 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
         with open(path, encoding='utf8') as csvfile:
             dict_reader = csv.DictReader(csvfile, delimiter=',')
             
-            edges, local_nodes = [], []
+            edges, l_nodes = [], []
             
             for row in dict_reader:
                 coordinates = re.findall(r"[-]?[0-9]*\.[0-9]*", row['geometry'])
@@ -112,20 +105,23 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
             DG = nx.DiGraph(
                 date_time = graph_properties['date_time'], 
                 level     = graph_properties['level'], 
-                tile_size = graph_properties['tile_size']
+                tile_size = graph_properties['tile_size'],
+                file      = path.name
             )
             
-            temp.append((DG, local_nodes, edges))
+            temp.append((DG, l_nodes, edges))
     
-    # Constructing final graph from graph object, nodes and edges and adding it to list graphs
-    for (graph, local_nodes, edges) in temp:
+    for (graph, l_nodes, edges) in temp:
         if(use_global_nodes):
-            graph.add_nodes_from(global_nodes)
+            graph.add_nodes_from(g_nodes)
         else:
-            graph.add_nodes_from(local_nodes)
+            graph.add_nodes_from(l_nodes)
         graph.add_edges_from(edges)
         graphs.append(graph)
-            
+    
+    end = timer()
+    if (display_runtime): print(f'Runtime: {end - start} seconds')
+    
     return graphs if graphs else None
     
 def save_movement_graph_to_file():
