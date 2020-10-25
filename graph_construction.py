24b16de74e25ddedecc8185e152528202f2de104
PatrickVogt-MPI <68254176+PatrickVogt-MPI@users.noreply.github.com>
2020-10-25 02:37:50 +0100

    Add files via upload

--- graph_construction.py
+++ graph_construction.py
@@ -1,65 +1,44 @@
 # Definitions:
-# Nodes are defined as tuples: node = (id: int, property: dict)
-#   id is the unique name of the node
+# Nodes are defined as tuples: node = (id: (lat: float, lon: float)), property: dict)
+#   id is the unique (lat, lon) tuple of the node and serves as identifier
 #   property contains the node attributes derived from the .csv data set
-# Edges are defined as 3-tuples: edge = (id1: int, id2: int, property: dict)
-#   id1 is the unique name of the start node
-#   id2 is the unique name of the end node
+# Edges are defined as 3-tuples: edge = (id1: (lat: float, lon: float), id2: (lat: float, lon: float), property: dict)
+#   id1 is the id of the start node
+#   id2 is the id of the end node
 #   property contains the edge attributes derived from the .csv data set
 
 import csv
 import networkx as nx
+import numpy as np
 import re
-import sys
 from pathlib import Path
 from timeit import default_timer as timer
 
-def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_nodes: bool = False, display_runtime: bool = False) -> list:
+def create_movement_graphs_from_csv(path: Path, key: str = None, allow_loops = True, negate_weight = False, remove_isolates = False, display_runtime: bool = False) -> list:
     '''
     Creates a list of graph data structures from all .csv files found in directory <path>
 
     Args:
         path:   Path pointing to the data directory of .csv files
-        key:    Search key for files
+        key:    Search key for file names
         
     Returns:
         graphs: List of graph data structures created from each .csv file at the <path> directory. Returns 'None' instead, if empty
     '''
-    def add_unique_node(t_prop: dict) -> int:
-        '''
-        Adds node with properties 't_node' to list 'g_nodes' if unique and in any case
-        adds it to list 'l_nodes' for graph construction and returns its identifier.
-        In this code, t stands for test, g for global and  l for local.
-
-        Args:
-            t_node: properties of the test node
-
-        Returns:
-            g_id:   identifier of global node
-            t_id:   identifier of local node
-        '''
-        for g_id, g_prop in g_nodes:
-            if(g_prop['lat'] == t_prop['lat'] and g_prop['lon'] == t_prop['lon']):
-                l_nodes.append((g_id, g_prop))
-                return g_id
-        t_id = len(g_nodes) + 1
-        g_nodes.append((t_id, t_prop))
-        l_nodes.append((t_id, t_prop))
-        return t_id
     
     start = timer()
     
-    # Introduction of global_nodes causes consistent identifier for all nodes among all graphs from different .csv files
-    graphs, g_nodes, temp = [], [], []
-    
     pattern = '*' + key + '*.csv' if key else '*.csv'
     csv_paths = [csv_path for csv_path in Path(path).glob(pattern)]
+    
+    graphs = []
     
     for path in csv_paths:  
         with open(path, encoding='utf8') as csvfile:
             dict_reader = csv.DictReader(csvfile, delimiter=',')
             
-            edges, l_nodes = [], []
+            edges, nodes = [], []
+            n_total      = 0
             
             for row in dict_reader:
                 coordinates = re.findall(r"[-]?[0-9]*\.[0-9]*", row['geometry'])
@@ -67,11 +46,11 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
                 graph_properties = {
                     'date_time': row['date_time'],
                     'level':     row['level'],
-                    'tile_size': int(row['tile_size'])
+                    'tile_size': int(row['tile_size']),
+                    'file':      path.name
                 }
+                start_node_id = (float(coordinates[1]), float(coordinates[0])) # (lat, lon)
                 start_node_properties = {
-                    'lat':          float(coordinates[0]),
-                    'lon':          float(coordinates[1]),
                     'polygon_lat':  float(row['start_lat']),
                     'polygon_lon':  float(row['start_lon']),
                     'polygon_id':   int(row['start_polygon_id']),
@@ -79,9 +58,8 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
                     'country':      row['country']
                     #'quadkey':      int(row['start_quadkey'])
                 }
+                end_node_id = (float(coordinates[3]), float(coordinates[2]))   # (lat, lon)
                 end_node_properties = {
-                    'lat':          float(coordinates[2]),
-                    'lon':          float(coordinates[3]),
                     'polygon_lat':  float(row['end_lat']),
                     'polygon_lon':  float(row['end_lon']),
                     'polygon_id':   int(row['end_polygon_id']),
@@ -90,6 +68,7 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
                     #'quadkey':      int(row['end_quadkey'])
                 }
                 edge_properties = {
+                    'weight':         None,
                     'n_crisis':       int(row['n_crisis']),
                     'n_baseline':     float(row['n_baseline']),
                     'length_km':      float(row['length_km']),
@@ -97,30 +76,40 @@ def create_movement_graphs_from_csv(path: Path, key: str = None, use_global_node
                     'z_score':        float(row['z_score']),
                     'percent_change': float(row['percent_change']),
                 }
-      
-                start_node = add_unique_node(start_node_properties)
-                end_node = add_unique_node(end_node_properties)
-                edges.append((start_node, end_node, edge_properties))
+                
+                n_total += edge_properties['n_crisis']
+                
+                start_node = (start_node_id, start_node_properties)
+                end_node = (end_node_id, end_node_properties)
+                
+                nodes.extend([start_node, end_node])
+                edges.append((start_node_id, end_node_id, edge_properties))
+                       
+            graph = nx.DiGraph(**graph_properties)
+            graph.add_nodes_from(nodes)
+            graph.add_edges_from(edges)
             
-            DG = nx.DiGraph(
-                date_time = graph_properties['date_time'], 
-                level     = graph_properties['level'], 
-                tile_size = graph_properties['tile_size'],
-                file      = path.name
-            )
-            
-            temp.append((DG, l_nodes, edges))
-    
-    for (graph, l_nodes, edges) in temp:
-        if(use_global_nodes):
-            graph.add_nodes_from(g_nodes)
-        else:
-            graph.add_nodes_from(l_nodes)
-        graph.add_edges_from(edges)
-        graphs.append(graph)
+            # Add weight
+            adj_matrix = nx.to_dict_of_dicts(graph)            
+            for origin in graph.nodes:
+                sum = 0
+                for target in adj_matrix[origin]:
+                    sum += graph[origin][target]['n_crisis']
+                for target in adj_matrix[origin]:
+                    weight = graph[origin][target]['n_crisis']/sum
+                    #graph[origin][target]['weight'] = -weight if negate_weight else weight
+                    graph[origin][target]['weight'] = 1
+                    
+            if(not allow_loops):
+                graph.remove_edges_from(nx.selfloop_edges(graph))
+            if(remove_isolates):
+                graph.remove_nodes_from(list(nx.isolates(graph)))
+         
+            graphs.append(graph)
     
     end = timer()
-    if (display_runtime): print(f'Runtime: {end - start} seconds')
+    
+    if (display_runtime): print(f'Runtime Graph Creation: {end - start} seconds')
     
     return graphs if graphs else None
     
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 settings = {
-    'data_dir': r'D:\Eigene Dokumente\Arbeit\Studium\Bachelorarbeit\Graph Analysis\data',
+    'data_dir': r'D:\Eigene Dokumente\Arbeit\Studium\Bachelorarbeit\map data\Sao Paulo Coronavirus Disease Prevention Map Apr 16 2020 Id Movement between Tiles',
+    #'data_dir': r'D:\Eigene Dokumente\Arbeit\Studium\Bachelorarbeit\Graph Analysis\data'
 }
