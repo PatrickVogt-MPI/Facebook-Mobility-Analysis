161bfde130e5067548258dd98796643ccba1bd97
PatrickVogt-MPI <68254176+PatrickVogt-MPI@users.noreply.github.com>
2020-12-31 16:46:45 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,4 +0,0 @@
-settings = {
-    'data_dir': r'D:\Eigene Dokumente\Arbeit\Studium\Bachelorarbeit\map data\Sao Paulo Coronavirus Disease Prevention Map Apr 16 2020 Id Movement between Tiles',
-    #'data_dir': r'D:\Eigene Dokumente\Arbeit\Studium\Bachelorarbeit\Graph Analysis\data'
-}
