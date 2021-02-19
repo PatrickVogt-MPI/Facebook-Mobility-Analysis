28aa4e7fb7b07af95ddd0808e50e814b6f11c70e
PatrickVogt-MPI <68254176+PatrickVogt-MPI@users.noreply.github.com>
2021-02-19 03:18:30 +0100

    Add files via upload

--- auto.py
+++ auto.py
@@ -1,21 +1,25 @@
 import keyboard
 import time
-
+'''
+Navigate to page with download links of target and move mouse pointer above title and run the program.
+Pass number of links as command line argument.
+'''
+# If time: Add automation, add robustness
 iterations = int(sys.argv[1])
 mouse.click()
 time.sleep(1)
 keyboard.send('ctrl+f')
 time.sleep(1)
 keyboard.send('2')
-time.sleep(2)
+time.sleep(1)
 keyboard.send('0')
-time.sleep(2)
+time.sleep(1)
 keyboard.send('2')
-time.sleep(2)
+time.sleep(1)
 keyboard.send('enter')
-time.sleep(2)
+time.sleep(1)
 
-for _ in range(iterations-2):
+for _ in range(iterations-1):
     keyboard.send('ctrl+enter')
     time.sleep(0.1)
     keyboard.send('ctrl+g')
